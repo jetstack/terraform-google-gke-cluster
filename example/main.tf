745816829be4fe6d3b896e1a9833fff82df829a0
Hays Clark <hays.clark@gmail.com>
2019-06-28 00:59:23 -0700

    Porting to support Terraform v0.12
    - performed 0.12upgrade and resolved various issues
    - 1 TF-UPGRADE-TODO remains to be resolved
    
    Signed-off-by: Hays Clark <hays.clark@gmail.com>

--- example/main.tf
+++ example/main.tf
@@ -19,26 +19,27 @@ terraform {
   required_version = "~> 0.11"
 
   # Use a GCS Bucket as a backend
-  backend "gcs" {}
+  backend "gcs" {
+  }
 }
 
 # Local values assign a name to an expression, that can then be used multiple
 # times within a module. They are used here to determine the GCP region from
 # the given location, which can be either a region or zone.
 locals {
-  gcp_location_parts = ["${split("-", var.gcp_location)}"]
-  gcp_region         = "${local.gcp_location_parts[0]}-${local.gcp_location_parts[1]}"
+  gcp_location_parts = split("-", var.gcp_location)
+  gcp_region         = format("%s-%s", local.gcp_location_parts[0], local.gcp_location_parts[1])
 }
 
 # https://www.terraform.io/docs/providers/google/index.html
 provider "google" {
   version = "2.5.1"
-  project = "${var.gcp_project_id}"
-  region  = "${local.gcp_region}"
+  project = var.gcp_project_id
+  region  = local.gcp_region
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "${var.vpc_network_name}"
+  name                    = var.vpc_network_name
   auto_create_subnetworks = "false"
 }
 
@@ -52,28 +53,26 @@ resource "google_compute_subnetwork" "vpc_subnetwork" {
   # a dash, lowercase letter, or digit, except the last character, which
   # cannot be a dash.
   #name = "default-${var.gcp_cluster_region}"
-  name = "${var.vpc_subnetwork_name}"
+  name = var.vpc_subnetwork_name
 
-  ip_cidr_range = "${var.vpc_subnetwork_cidr_range}"
+  ip_cidr_range = var.vpc_subnetwork_cidr_range
 
   # The network this subnet belongs to. Only networks that are in the
   # distributed mode can have subnetworks.
-  network = "${var.vpc_network_name}"
+  network = var.vpc_network_name
 
   # An array of configurations for secondary IP ranges for VM instances
   # contained in this subnetwork. The primary IP of such VM must belong to the
   # primary ipCidrRange of the subnetwork. The alias IPs may belong to either
   # primary or secondary ranges.
-  secondary_ip_range = [
-    {
-      range_name    = "${var.cluster_secondary_range_name}"
-      ip_cidr_range = "${var.cluster_secondary_range_cidr}"
-    },
-    {
-      range_name    = "${var.services_secondary_range_name}"
-      ip_cidr_range = "${var.services_secondary_range_cidr}"
-    },
-  ]
+  secondary_ip_range {
+    range_name    = var.cluster_secondary_range_name
+    ip_cidr_range = var.cluster_secondary_range_cidr
+  }
+  secondary_ip_range {
+    range_name    = var.services_secondary_range_name
+    ip_cidr_range = var.services_secondary_range_cidr
+  }
 
   # When enabled, VMs in this subnetwork without external IP addresses can
   # access Google APIs and services by using Private Google Access. This is
@@ -81,31 +80,31 @@ resource "google_compute_subnetwork" "vpc_subnetwork" {
   private_ip_google_access = true
 
   depends_on = [
-    "google_compute_network.vpc_network",
+    google_compute_network.vpc_network,
   ]
 }
 
 module "cluster" {
-  source  = "jetstack/gke-cluster/google"
-  version = "0.1.0-beta2"
+  source  = "../terraform-google-gke-cluster"
+  #version = "0.2.0-alpha1"
 
   # These values are set from the terrafrom.tfvas file
-  gcp_project_id                         = "${var.gcp_project_id}"
-  cluster_name                           = "${var.cluster_name}"
-  gcp_location                           = "${var.gcp_location}"
-  daily_maintenance_window_start_time    = "${var.daily_maintenance_window_start_time}"
-  node_pools                             = "${var.node_pools}"
-  cluster_secondary_range_name           = "${var.cluster_secondary_range_name}"
-  services_secondary_range_name          = "${var.services_secondary_range_name}"
-  master_ipv4_cidr_block                 = "${var.master_ipv4_cidr_block}"
-  access_private_images                  = "${var.access_private_images}"
-  http_load_balancing_disabled           = "${var.http_load_balancing_disabled}"
-  master_authorized_networks_cidr_blocks = "${var.master_authorized_networks_cidr_blocks}"
+  gcp_project_id                         = var.gcp_project_id
+  cluster_name                           = var.cluster_name
+  gcp_location                           = var.gcp_location
+  daily_maintenance_window_start_time    = var.daily_maintenance_window_start_time
+  node_pools                             = var.node_pools
+  cluster_secondary_range_name           = var.cluster_secondary_range_name
+  services_secondary_range_name          = var.services_secondary_range_name
+  master_ipv4_cidr_block                 = var.master_ipv4_cidr_block
+  access_private_images                  = var.access_private_images
+  http_load_balancing_disabled           = var.http_load_balancing_disabled
+  master_authorized_networks_cidr_blocks = var.master_authorized_networks_cidr_blocks
 
   # Refer to the vpc-network and vpc-subnetwork by the name value on the
   # resource, rather than the variable used to assign the name, so that
   # Terraform knows they must be created before creating the cluster
-  vpc_network_name = "${google_compute_network.vpc_network.name}"
 
-  vpc_subnetwork_name = "${google_compute_subnetwork.vpc_subnetwork.name}"
+  vpc_network_name    = google_compute_network.vpc_network.name
+  vpc_subnetwork_name = google_compute_subnetwork.vpc_subnetwork.name
 }
